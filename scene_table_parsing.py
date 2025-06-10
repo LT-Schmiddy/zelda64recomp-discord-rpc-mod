ad75e31bc8ff4402a51e48646a06cf28246d0bf7
LT-Schmiddy <schmidalex1996@gmail.com>
2025-06-09 23:23:36 -0400

    Analysis script for scene data

---
+++ scene_table_parsing.py
@@ -0,0 +1,23 @@
+import csv, json
+
+scene_data_names = ['id', 'entrance', 'name', 'translation', 'display_name', "what?"]
+scene_entries = {}
+
+with open('scene_table.csv', newline='') as csvfile:
+    scene_reader = csv.reader(csvfile, delimiter='\t', quotechar='|')
+    for row in scene_reader:
+        # print(', '.join(row))
+
+        scene_data = {}
+        for i in range(0, len(row)):
+            scene_data[scene_data_names[i]] = row[i]
+        
+        scene_entries[int(scene_data['id'], 16)] = scene_data
+        
+for id in range(0, int("6D", 16) + 1):
+    display_name = "Unknown Scene"
+    
+    if id in scene_entries:
+        if "display_name" in scene_entries[id]:
+            display_name = scene_entries[id]["display_name"]
+    print(f"{{ {id}, \"{display_name}\" }},")
